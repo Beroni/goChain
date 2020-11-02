32f29e36f420f5775b465c1ef48b6ff9f8f169fc
Beroni <beroni@getty.io>
2020-11-02 15:45:25 -0300

    Simple Blockchain

--- main.go
+++ main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 )
 
+type Blockchain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, newBlock)
+}
+
+func CreateGenesisBlock() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{CreateGenesisBlock()}}
+
+}
+
 func main() {
-	fmt.Println("Hello, World")
+
+	chain := InitBlockchain()
+
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+
 }
